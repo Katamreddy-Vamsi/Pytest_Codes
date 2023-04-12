5002cfc343fdfec76651095aea3b239ae77527e1
Malot <Dhawal.Malot@sony.com>
2023-04-12 13:06:42 +0530

    Changed Abhi.py

---
+++ Abhi.py
@@ -0,0 +1,27 @@
+def func(a, b):
+    c = a + b
+    print("sum of two variable : ", c)
+
+
+
+
+print("Hello from Dhawal Malot.")
+
+print("sum of two variable : ", func(5, 6))
+print(func(5,8))
+
+# func(5, 6)
+
+print(func(5, 6))
+print(func(3,4))
+
+func(5, 6)
+print("hello")
+
+
+
+
+# func(6, 7)
+
+
+print(func(3, 5))
