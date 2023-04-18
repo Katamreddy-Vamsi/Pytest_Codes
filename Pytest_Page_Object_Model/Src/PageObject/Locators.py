47258ee6afe8a7941adc300b2ab1463d4c5e497f
Singh2 <Abhishek.Singh2@sony.com>
2023-04-18 13:03:45 +0530

    Adding POM Folder

---
+++ Pytest_Page_Object_Model/Src/PageObject/Locators.py
@@ -0,0 +1,6 @@
+class Locator(object):
+    # Google Search Page
+    search_text = "//textarea[@name='q']"
+    submit = "//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']"
+    # //input[@name='Google Search']"
+    # logo = "//*[@id='hplogo']"
