2996293229578a58548a092b6487a9c2422530fa
Janakallu <Roopashree.Janakallu@sony.com>
2023-04-21 16:17:49 +0530

    commit1

--- Roopanew/Pytest_Page-Object-Model/Pytest_Page_Object_Model/test_sample.py
+++ Roopanew/Pytest_Page-Object-Model/Pytest_Page_Object_Model/test_sample.py
@@ -5,12 +5,8 @@ def func(x):
 def test_answer():
     assert func(3) == 6 , "Run assert"
 
-def method(x):
-    return x + 1
-
 def ans():
-    assert method(4)==5 , "assert pass x y are same "
-    assert method(5)==-1 , "assert failed"
-
-
+    assert func(4) == 5 , "assert pass x y are same "
+    assert func(5) == -1 , "assert failed"
+print(" ")
 
